c08add7b7e852979fa1a40ed14a58fc31897523c
cafedeaqua <cafedeaqua@users.noreply.github.com>
2015-02-15 12:27:19 +0900

    Change led logic

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ periodicActivity(); //call the periodicActivity function
 function periodicActivity()
 {
   myOnboardLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
-  myOnboardLed2.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
+  myOnboardLed2.write(ledState?0:1); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
   ledState = !ledState; //invert the ledState
   setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
 }
